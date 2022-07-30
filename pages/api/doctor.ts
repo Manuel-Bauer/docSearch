ed96c65c6238df341301187de9711b520875ce2b
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-07-30 21:04:55 +0200

    doctor api file added

---
+++ pages/api/doctor.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Prisma, Doctor } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+
+// import { prisma } from '../../db';
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log('req.body', req.body);
+  if (req.method === 'POST') {
+    const doctor = req.body;
+    console.log('doctor', doctor);
+    const createDoctor = await prisma.doctor.create({ data: doctor });
+    return res.status(200).json(createDoctor);
+  }
+}
---
+++ utils/apiService/doctorUtil.tsx
@@ -0,0 +1,15 @@
+import { Doctor, Prisma } from '@prisma/client';
+
+export async function createDoctor(
+  item: Prisma.DoctorCreateInput
+): Promise<Doctor> {
+  console.log('createDoctor', item);
+  const res = await fetch('../../pages/api/doctor', {
+    method: 'POST',
+    body: JSON.stringify(item),
+  });
+  const data = await res.json();
+  return data;
+}
+
+//t
